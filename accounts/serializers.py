0b7bf4f4f47a94076131120f766bc54e4113face
NeerajNaik <neerajnaik125@gmail.com>
2021-04-29 19:25:11 +0530

    'serializers'

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,7 +14,7 @@ class ExamSerializer(serializers.ModelSerializer):
 class ExamDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exam_Data
-        fields = ('exam_id','tot_blocks','tot_departments','reliever_duty','extra_blocks')        
+        fields = ('exam_id','tot_blocks','tot_departments','reliever_duty','extra_blocks','InterorIntra')        
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
